ec8f6ad2c4d1264624e0e88d5cd92300a0c079af
Kitsune Solar <kitsune.solar@gmail.com>
2018-06-09 14:17:55 +0300

    Update resource

---
+++ MW_EXT_Key.class.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Class MW_EXT_Key
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+class MW_EXT_Key {
+
+	/**
+	 * Clear DATA (escape html).
+	 *
+	 * @param $string
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	private static function clearData( $string ) {
+		$outString = htmlspecialchars( trim( $string ), ENT_QUOTES );
+
+		return $outString;
+	}
+
+	/**
+	 * Register tag function.
+	 *
+	 * @param Parser $parser
+	 *
+	 * @return bool
+	 * @throws MWException
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( 'key', __CLASS__ . '::onRenderTag' );
+
+		return true;
+	}
+
+	/**
+	 * Render tag function.
+	 *
+	 * @param $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onRenderTag( $input, $args = [], Parser $parser, PPFrame $frame ) {
+		// Get content.
+		$getContent = trim( $input );
+		$outContent = $getContent;
+
+		// Out HTML.
+		$outHTML = '<kbd class="mw-ext-key">' . $outContent . '</kbd>';
+
+		// Out parser.
+		$outParser = $outHTML;
+
+		return $outParser;
+	}
+
+	/**
+	 * Load resource function.
+	 *
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 *
+	 * @return bool
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+		$out->addModuleStyles( array( 'ext.mw.key.styles' ) );
+
+		return true;
+	}
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Информация
+
+Выделение кнопки клавиатуры, либо кнопки функционального окна приложения.
+
+## Синтаксис
+
+```
+<key>CTRL</key>+<key>ALT</key>+<key>DEL</key>
+```
+
+## Установка
+
+1. Загрузите папки и файлы в директорию `extensions/MW_EXT_Key`.
+2. В самый низ файла `LocalSettings.php` добавьте строку:
+
+```
+wfLoadExtension( 'MW_EXT_Key' );
+```
+
+## Авторы
+
+- [**Kitsune Solar**](https://kitsune.solar/) - разработчик.
+
+## Ссылки
+
+- [**METASTORE**](https://metastore.pro/) - хранилище открытых проектов [**METADATA**](https://metadata.foundation/).
+- [**METADATA / Foundation**](https://metadata.foundation/) - поддержка и разработка открытых проектов.
